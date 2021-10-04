e342f9cb922e378158d70687b3a1a1d5c37d2b5a
egioacchini346 <eag346@drexel.edu>
2021-10-04 05:23:27 -0400

    hero h1

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,42 +5,5 @@ import {
 const mainTL = gsap.timeline();
 // reference the timeline | Do we want to start at the beginning or the end of the timeline? | What thing do you want to annimate? | Duration of the animation? | What do we want to do?
 
-mainTL.from("#hero", {
-    duration: 2,
-    alpha: 0,
-    sacle: 2
-});
+mainTL.from("#hero h1", {duration: 2,alpha: 0,scale:2});
 
-const state = Flop.getState(".logo");
-
-
-gsap.set(".quickflip", {
-    transformStyle: "preserve-3d",
-    transformPerspective: 1000
-});
-gsap.set(".logo", {
-    transformStyle: "preserve-3d",
-    transformOrigin: "50% 50%",
-});
-
-
-const timing = 1;
-
-const tl = gsap.timeline({
-    paused: true
-});
-tl.to(".qf-card", {
-    rotationY: "+=180",
-    duration: timing
-});
-tl.to(".quickflip", {
-    z: 50,
-    duration: timing / 2,
-    yoyo: true,
-    repeat: 1
-}, 0);
-
-gsap.utils.toArray(".quickflip").forEach(flip => {
-    flip.addEventListener("mouseenter", event => tl.play(logo));
-    flip.addEventListener("mouseleave", event => tl.reverse(logo));
-});
