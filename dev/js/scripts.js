d7ff0c95d8666c76a1ab4261e807bea1ba6838ea
egioacchini346 <eag346@drexel.edu>
2021-10-04 04:51:15 -0400

    js update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,4 @@
-import {
-    gsap
-} from "gsap";
+import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 // reference the timeline | Do we want to start at the beginning or the end of the timeline? | What thing do you want to annimate? | Duration of the animation? | What do we want to do?
