80c31a076f9aed89c66d23a36990ec307bd211d8
egioacchini346 <eag346@drexel.edu>
2021-10-04 05:39:25 -0400

    animations

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,11 +7,34 @@ const mainTL = gsap.timeline();
 
 
 const heroHeight = document.querySelector("#hero");
-const aniTime = 0.5;
+const aniTime = 4.0;
 
 console.log(heroHeight.clientHeight + "px is the height of the hero section");
 console.log(heroHeight.clientHeight + "px is the width of the hero section");
 
-mainTL.from("#hero hero-content", {duration:2, alpha:0, y: heroHeight.clientHeight})
-    .from("#hero h1", {duration:1, y:"+=380", alpha:0, rotation:180}, "madeUp")
-    .from("#hero h2", {duration:3, y:"+=380", alpha:0, rotation:180}, "madeUp")
+mainTL.from("#hero hero-content", {
+        duration: 2,
+        alpha: 0,
+        y: heroHeight.clientHeight
+    })
+    .from("#hero h1", {
+        duration: 1,
+        y: "+=380",
+        alpha: 0,
+        rotation: 180
+    }, "madeUp")
+    .from("#hero h2", {
+        duration: 3,
+        y: "+=380",
+        alpha: 0,
+        rotation: 180
+    }, "madeUp")
+
+
+const state = Flip.getState(".logo");
+Flip.from(state, {
+    duration: 1,
+    ease: "power1.inOut",
+    absolute: true,
+    onComplete: myFunc
+});
