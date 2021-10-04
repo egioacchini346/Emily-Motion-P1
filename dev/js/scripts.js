e49fbc92fa214f4de98faae34e720965e1df7349
egioacchini346 <eag346@drexel.edu>
2021-10-04 05:24:00 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,5 +5,8 @@ import {
 const mainTL = gsap.timeline();
 // reference the timeline | Do we want to start at the beginning or the end of the timeline? | What thing do you want to annimate? | Duration of the animation? | What do we want to do?
 
-mainTL.from("#hero h1", {duration: 2,alpha: 0,scale:2});
-
+mainTL.from("#hero h1", {
+    duration: 2,
+    alpha: 0,
+    scale: 2
+});
