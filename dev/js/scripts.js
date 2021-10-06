ab05ff163018b66b84192faf1fa8391d83ec343a
egioacchini346 <eag346@drexel.edu>
2021-10-06 01:35:04 -0400

    animations

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,10 +8,10 @@ const mainTL = gsap.timeline()
 //Const maniTime = 0.5;
 
 mainTL.from("#hero", {duration:2, alpha:0,x:1000})
-    .from("#h1", {duration:1, y:" += 300", alpha:0, rotation:180},"madeUp")
-    .from("#h2", {duration:3, y:" += 300 ", alpha:0, rotation:180},"madeUp")
+    .from("#h1", {duration:1, y:"+= 300", alpha:0, rotation:180},"madeUp")
+    .from("#h2", {duration:3, y:"+= 300 ", alpha:0, rotation:180},"madeUp")
     .from("#logo", {duration:3, y:"+=300", alpha:0, rotation:180}, "madeUp")
-    .from("#pin", {duration:3, y:"+=300", alpha:0, rotation:180}, "madeUp")
+    .from("#pin", {duration: 1, x: 3, scale: 1.5,ease: 'bounce'})
     .from("#b1", {duration:3, y:"+=300", alpha:0}, "madeUp")
     .from("#b2", {duration:3, y:"+=300", alpha:0}, "madeUp")
     .from("#b3", {duration:3, y:"+=300", alpha:0}, "madeUp")
