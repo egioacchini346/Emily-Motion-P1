868ba4c3b41d23ee023c7def9647f73967bfc120
egioacchini346 <eag346@drexel.edu>
2021-12-06 08:03:18 -0500

    updated kit and scss and scripts

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,13 +8,17 @@ gsap.registerPlugin(GSDevTools, MotionPathPlugin, DrawSVGPlugin);
 
 gsap.set("#card",{ scale: .7, transformOrigin:"center"});
 gsap.set(".window",{ opacity: 0, transformOrigin:"center"});
-
+gsap.set(".house",{ opacity: 0, transformOrigin:"center"});
 
 const mainTL = gsap.timeline()
 
 function window1(){
     const tl=gsap.timeline();
     tl.to(".window",{ opacity: 1, duration: 2});
+    tl.from("#snow-top-right-window",{ opacity: 0, duration: .7}), "sametime";
+    tl.to(".house",{ opacity: 1, duration: .7}), "sametime";
+    
+
     return tl;
 }
 
