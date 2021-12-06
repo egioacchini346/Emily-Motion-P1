ed34bc42b3e6006f441a1157af31e696f3d59d01
egioacchini346 <eag346@drexel.edu>
2021-12-06 07:21:39 -0500

    more changed files
    card.kit, variables, _card.scss, scripts

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,8 +6,20 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(GSDevTools, MotionPathPlugin, DrawSVGPlugin);
 
+gsap.set("#card",{ scale: .7, transformOrigin:"center"});
+gsap.set(".window",{ opacity: 0, transformOrigin:"center"});
 
-    
+
+const mainTL = gsap.timeline()
+
+function window1(){
+    const tl=gsap.timeline();
+    tl.to(".window",{ opacity: 1, duration: 2});
+    return tl;
+}
+
+mainTL.add(window1())
+
 GSDevTools.create();
 MotionPathPlugin.create();
 DrawSVGPlugin.create();
