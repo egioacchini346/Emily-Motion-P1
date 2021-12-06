0166f0a30eadbd63778d8318d7e01f3f7451204d
egioacchini346 <eag346@drexel.edu>
2021-12-06 16:24:42 -0500

    push updates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,6 +12,7 @@ gsap.set(".window",{ opacity: 0, transformOrigin:"center"});
 gsap.set(".house",{ opacity: 0, transformOrigin:"center"});
 gsap.set("#Frame-5",{ opacity: 0, transformOrigin:"center"});
 gsap.set(".green",{ scale: 8, transformOrigin:"center"});
+gsap.set(".colleges",{ opacity: 0, transformOrigin:"center"});
 //gsap.set("#Frame-14",{ opacity:0, transformOrigin:"center"});
 
 
@@ -30,31 +31,32 @@ function window1(){
 function frame14(){
     const tl=gsap.timeline();
 
-    tl.to("#animation",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#architect-studies",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#history",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#museum",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#dance",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#designresearch",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#designmerch",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#digital",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#entertainment",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#fashion",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#film",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#game",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#graphic",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#interiord",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#interiora",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#interiorad",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#music",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#photography",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#product",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#retail",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#screenwriting",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#television",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#virtualreality",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#userexperience",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#urbanstrategy",{duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#animation",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#architectstudies",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#architecture",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#history",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#museum",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#dance",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#designresearch",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#designmerch",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#digital",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#entertainment",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#fashion",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#film",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#game",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#graphic",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#interiord",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#interiora",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#interiorad",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#music",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#photography",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#product",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#retail",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#screenwriting",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#television",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#virtualreality",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#userexperience",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#urbanstrategy",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
     return tl;
 }
 mainTL.add(window1())
