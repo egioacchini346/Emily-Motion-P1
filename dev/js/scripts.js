82bd093a0ccddf28f177215a7f86369493609bb9
egioacchini346 <eag346@drexel.edu>
2021-12-07 09:26:19 -0500

    updates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,6 +12,7 @@ gsap.set(".colleges",{ opacity: 0, transformOrigin:"center"});
 
 
 
+
 const mainTL = gsap.timeline()
 
 function windowscene(){
@@ -23,36 +24,60 @@ function windowscene(){
 function cabinscene(){
     const tl=gsap.timeline();
 
-    tl.to("#animation",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#architectstudies",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#architecture",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#history",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#museum",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#dance",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#designresearch",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#designmerch",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#digital",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#entertainment",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#fashion",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#film",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#game",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#graphic",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#interiord",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#interiora",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#interiorad",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#music",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#photography",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#product",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#retail",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#screenwriting",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#television",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#virtualreality",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#userexperience",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
-    tl.to("#urbanstrategy",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to(".colleges",{duration: .3, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
     return tl;
 }
+
+function windowscene2(){
+    const tl=gsap.timeline();
+    
+    return tl;
+}
+
+function fireplacescene(){
+    const tl=gsap.timeline();
+    tl.from("#1", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#2", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#3", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#4", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#5", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#6", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#7", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#8", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#9", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#10", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#11", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#12", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#13", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#14", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#15", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#16", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#17", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#18", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#19", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#20", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#21", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#22", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#23", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#24", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#25", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#26", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#27", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#28", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#29", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#30", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#31", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.from("#32", {duration: .15, scale: .1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    //tl.to(cs, 0.6, {tint: 0x228200, ease: Power4.easeOut})
+    return tl;
+}
+
+
+
 mainTL.add(windowscene())
 mainTL.add(cabinscene())
+mainTL.add(windowscene2())
+mainTL.add(fireplacescene())
 
 GSDevTools.create();
 MotionPathPlugin.create();
